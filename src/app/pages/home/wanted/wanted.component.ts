f942cc5f569441d8b8415901581970ff43169aed
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-02-09 16:05:07 -0500

    finishing second slider

--- src/app/pages/home/wanted/wanted.component.ts
+++ src/app/pages/home/wanted/wanted.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-wanted',
   templateUrl: './wanted.component.html',
@@ -7,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WantedComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+
 
 }
