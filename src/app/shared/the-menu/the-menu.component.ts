88a53bbbac107e15652ea7866db60f9af1d4493e
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-07 09:34:33 -0500

    cambios

--- src/app/shared/the-menu/the-menu.component.ts
+++ src/app/shared/the-menu/the-menu.component.ts
@@ -1,30 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-the-menu',
-  templateUrl: './the-menu.component.html',
-  styleUrls: ['./the-menu.component.scss']
+  selector: "app-the-menu",
+  templateUrl: "./the-menu.component.html",
+  styleUrls: ["./the-menu.component.scss"],
 })
 export class TheMenuComponent implements OnInit {
-
-  public activeLang = "en"
+  public activeLang = "en";
   navbarOpen = false;
-
 
   constructor(private translate: TranslateService) {
     this.translate.setDefaultLang(this.activeLang);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public cambiarLenguaje(lang: any) {
     this.activeLang = lang;
     this.translate.use(lang);
   }
+
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
-
 }
