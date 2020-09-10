7add4041ea595ed632112fed38647d1cec558e34
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-09 21:50:19 -0500

    otro despliegue

--- src/app/pages/gallery/child/child.component.ts
+++ src/app/pages/gallery/child/child.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ChildComponent implements OnInit {
   public imgChild: string;
+  public desc: string;
+
   constructor() {
     this.imgChild = "https://i.postimg.cc/26bzXs3q/ninos.jpg";
+    this.desc =
+      "https://docs.google.com/presentatio\nn/d/1qpJT5w4oQsSdAHCoxtdrlszvTTqXl2bHb4n\nKKYk0MVg/mobilepresent?slide=id.g139f32e65b_1_95";
   }
 
   ngOnInit() {}
--- src/app/pages/gallery/woman/woman.component.ts
+++ src/app/pages/gallery/woman/woman.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from "@angular/core";
 })
 export class WomanComponent implements OnInit {
   public imgWoman: string;
+  public desc: string;
   constructor() {
     this.imgWoman = "https://i.postimg.cc/PJLWPRC1/GOLDEN-HEART-TUNIC.jpg";
+    this.desc =
+      "https://docs.google.com/presentation/d/1qpJT5w4\noQsSdAHCoxtd\nrlszvTTqXl2bHb4nKKYk0MVg\n/mobilepresent?s\nlide=id.g2bbdf077bf_0_20";
   }
 
   ngOnInit() {}
