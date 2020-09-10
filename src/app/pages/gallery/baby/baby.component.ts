cb1f2b7a508a70c5bb4c84b046142e17da52c6d1
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-09 20:50:27 -0500

    arreglando baby component

--- src/app/pages/gallery/baby/baby.component.ts
+++ src/app/pages/gallery/baby/baby.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-baby',
-  templateUrl: './baby.component.html',
-  styleUrls: ['./baby.component.scss']
+  selector: "app-baby",
+  templateUrl: "./baby.component.html",
+  styleUrls: ["./baby.component.scss"],
 })
 export class BabyComponent implements OnInit {
+  public imgBaby: string;
+  public desc: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.imgBaby = "https://i.postimg.cc/FsRwNyhv/bbs.png";
+    this.desc =
+      "https://docs.google.com/presentation/d/1Cx4x1FJ2nByN7b9LAeQkZpVG15GDDvUrgvnR-Ym7FbE/edit?ts=5f5160af#slide=id.g18f2acd6d6_0_92";
   }
 
+  ngOnInit() {}
 }
