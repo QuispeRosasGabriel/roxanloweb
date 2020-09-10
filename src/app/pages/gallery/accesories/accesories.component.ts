6a4d1bf85f63a1456bd0b2a48de50f5f0f751a45
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-09 21:45:54 -0500

    link

--- src/app/pages/gallery/accesories/accesories.component.ts
+++ src/app/pages/gallery/accesories/accesories.component.ts
@@ -11,7 +11,7 @@ export class AccesoriesComponent implements OnInit {
   constructor() {
     this.imgAccesories = "https://i.postimg.cc/9XgFGNdc/PORTA-LAPTOP-RED.jpg";
     this.desc =
-      "https://docs.google.com/presentation/d/1KYyvGGNq6SAzZlfniI82kcuoperNnpogybJ9Swq47to/edit?ts=5f515fb4#slide=id.g6566e6322c_2_80";
+      "https://docs.google.com/presentation/d/1KYy\nvGGNq6\nSAzZlf\nniI82kcuoperN\nnpogybJ9Swq47to/\nedit?ts=5f515fb4#slide=id.\ng6566e6322c_2_80";
   }
 
   ngOnInit() {}
