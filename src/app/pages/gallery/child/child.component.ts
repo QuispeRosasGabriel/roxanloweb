f93f04c0d1aa54e48b1bd7e3e63f6ccee7c5ebe4
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-15 11:21:09 -0500

    cambios

--- src/app/pages/gallery/child/child.component.ts
+++ src/app/pages/gallery/child/child.component.ts
@@ -10,7 +10,7 @@ export class ChildComponent implements OnInit {
   public desc: string;
 
   constructor() {
-    this.imgChild = "https://i.postimg.cc/26bzXs3q/ninos.jpg";
+    this.imgChild = "https://i.postimg.cc/QxkGyZsc/ninas2.jpg";
     this.desc =
       "https://docs.google.com/presentatio\nn/d/1qpJT5w4oQsSdAHCoxtdrlszvTTqXl2bHb4n\nKKYk0MVg/mobilepresent?slide=id.g139f32e65b_1_95";
   }
