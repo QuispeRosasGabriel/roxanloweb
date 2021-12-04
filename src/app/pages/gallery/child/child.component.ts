35523608ab284b893c2d6ab071170844afc6cc77
QuispeRosasGabriel <gabrielomarquisperosas@gmail.com>
2021-12-03 21:53:05 -0500

    fix images and add new gallery

--- src/app/pages/gallery/child/child.component.ts
+++ src/app/pages/gallery/child/child.component.ts
@@ -10,9 +10,10 @@ export class ChildComponent implements OnInit {
   public desc: string;
 
   constructor() {
-    this.imgChild = "https://i.postimg.cc/QxkGyZsc/ninas2.jpg";
+    // this.imgChild = "https://i.postimg.cc/QxkGyZsc/ninas2.jpg";
+    this.imgChild = "https://i.postimg.cc/tgPWyHMb/Dress-and-Cardigan-Naomi.jpg";
     this.desc =
-      "https://docs.google.com/presentatio\nn/d/1qpJT5w4oQsSdAHCoxtdrlszvTTqXl2bHb4n\nKKYk0MVg/mobilepresent?slide=id.g139f32e65b_1_95";
+      "https://docs.google.com/presentatio\nn/d/1qpJT5w4oQsSdAH\nCoxtdrlszvTTqXl2bHb4n\nKKYk0MVg/mobilepresent?slide=id.g139f32e65b_1_95";
   }
 
   ngOnInit() {}
